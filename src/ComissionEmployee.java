9602af7f365dca82ed88e4987257b2495a2683d5
Vini Speck <vinicius.speck@catolicasc.org.br>
2020-11-03 01:44:10 -0300

    Initial commit

--- src/ComissionEmployee.java
+++ src/ComissionEmployee.java
@@ -4,13 +4,15 @@ public class ComissionEmployee extends Object{
     private final String firstName;
     private final String lastName;
     private final String cpf;
+    private double baseSalary;
     private double grossSales; //venda bruta
     private double comissionRate; //taxa de comissao
 
-    public ComissionEmployee(String firstName, String lastName, String cpf, double grossSales, double comissionRate) {
+    public ComissionEmployee(String firstName, String lastName, String cpf, double baseSalary, double grossSales, double comissionRate) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.cpf = cpf;
+        setBaseSalary(baseSalary);
         setGrossSales(grossSales);
         setComissionRate(comissionRate);
     }
@@ -27,12 +29,23 @@ public class ComissionEmployee extends Object{
         return cpf;
     }
 
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
     public double getGrossSales() {
         return grossSales;
     }
 
     public double getComissionRate() {
         return comissionRate;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        if(baseSalary < 0){
+            throw new IllegalArgumentException("O valor de salário (baseSalary) deve ser > 0");
+        }
+        this.baseSalary = baseSalary;
     }
 
     public void setGrossSales(double grossSales) {
@@ -51,13 +64,14 @@ public class ComissionEmployee extends Object{
 
     //ganho semanal do funcionario
     public double earnings(){
-        return comissionRate * grossSales;
+        return baseSalary + (comissionRate * grossSales);
     }
     @Override
     public String toString() {
-        return String.format("%s: %s %s\n%s: %s\n%s: R$ %.2f\n%s: %.2f\n%s: R$ %.2f",
+        return String.format("%s: %s %s\n%s: %s\n%s: R$ %.2f\n%s: R$ %.2f\n%s: %.2f\n%s: R$ %.2f",
                 "Funcionário por comissão", firstName, lastName,
                 "CPF", cpf,
+                "Salário Base", baseSalary,
                 "Venda Bruta", grossSales,
                 "Comissão", comissionRate,
                 "Rendimento", earnings()
---
+++ src/ComissionEmployeeTest.java
@@ -0,0 +1,33 @@
+public class ComissionEmployeeTest {
+    public static void main(String[] args) {
+        ComissionEmployee employee = new ComissionEmployee(
+                "João", "Silva", "123.456.789-00", 1200, 10000, .1
+        );
+        System.out.println("Imprimindo dados por métodos get: ");
+        System.out.println("Nome: " + employee.getFirstName());
+        System.out.println("Sobrenome: " + employee.getLastName());
+        System.out.println("CPF: " + employee.getCpf());
+        System.out.println("Salário Bruto: " + employee.getBaseSalary());
+        System.out.println("Venda bruta: " + employee.getGrossSales());
+        System.out.println("Comissão: " + employee.getComissionRate());
+        System.out.println("Rendimento: " + employee.earnings());
+
+        try {
+            employee.setGrossSales(-1);
+        } catch(IllegalArgumentException ex){
+            System.out.println("Erro atualizando vendas brutas do funcionário: " + ex.getMessage());
+        }
+        try {
+            employee.setComissionRate(4);
+        } catch(IllegalArgumentException ex){
+            System.out.println("Erro atualizando comissão do funcionário: " + ex.getMessage());
+        }
+        try {
+            employee.setBaseSalary(-1);
+        } catch(IllegalArgumentException ex){
+            System.out.println("Erro atualizando salário do funcionário: " + ex.getMessage());
+        }
+        System.out.println("Atualizada a informação do funcionário: ");
+        System.out.println("" + employee);
+    }
+}
